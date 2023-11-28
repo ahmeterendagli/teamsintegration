b79e81ddd9c5dff49c304153affd629546adf181
ahmet.dagli <ahmet.dagli@karel.com.tr>
2023-11-28 13:48:22 +0300

    initial commit

---
+++ src/main/java/com/example/teamsintegration/TeamsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamsintegration/TeamsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
